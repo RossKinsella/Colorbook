15eaffdfca4a5088ee9c0cd60228ea2851a801b4
RossKinsella <rosskinsella0@gmail.com>
2014-09-20 20:54:07 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,31 @@
+#colorbook
+
+###Description
+```
+A project exploring relationships over time and the human condition.
+```
+
+###Setup
+```
+Use my data (message_data.csv)
+
+- OR -
+
+Download your facebook data.
+Insert the messages.htm file into the directory
+bundle install.
+ruby main.rb
+```
+
+###End Goal
+```
+A web page where each interaction you have on facebook is shwon cronologically.
+Each person you interact with will have a unique color and sound.
+
+The aesthetic I'm going for is melancholic.
+```
+
+###Influences
+```
+http://wefeelfine.org/
+```
---
+++ main.rb
@@ -0,0 +1,57 @@
+require 'nokogiri'
+require 'time'
+
+# Get the messages xml.
+f = File.open("messages.htm")
+doc = Nokogiri::XML(f)
+
+# Create a collection of all the thread items
+# |- Each thread represents a conversation partnet.
+threadsXml =  doc.xpath('/html/body/div/div/div')
+
+# Create a collection of each conversation partner.
+partnersXml = threadsXml.xpath('text()')
+
+# Get the time of every message
+# |- Store the messages in a 2D array [partners][messages-of-that-partner]
+messageTimes = Array.new
+threadsXml.each_with_index { |conversationThread, index|
+	# Create a temporary array for the messages with this partner.
+	currentPartnerMessageTimes = Array.new
+	# Get the time of every message with this partner.
+	currentConversationPartnerMessageTimes = conversationThread.xpath('div/div/span[2]/text()')
+	
+	currentConversationPartnerMessageTimes.each do |messageTime|
+		currentPartnerMessageTimes << Time.parse(messageTime)
+	end
+	messageTimes << currentPartnerMessageTimes
+}
+
+# Assign a color to every conversation partner.
+colors = Array.new
+partnersXml.each do |partner|
+	colors << ("%06x" % (rand * 0xffffff))
+end
+
+
+# Bring the time and partner color data together
+# |- Store this data in a CSV manner -> [datetime, user_id, color]
+messagesWithUserId = Array.new
+for i in 0..partnersXml.count-1
+	messageTimes[i].each do |messageTime|
+		messagesWithUserId << (messageTime.to_s << ", " << i.to_s << ", #" << colors[i])
+	end
+end
+
+# Sort by datetime.
+messagesWithUserId = messagesWithUserId.sort
+
+# Write to csv.
+File.open('message_data.csv', 'w') do |f2|  
+  f2.puts "date_time, user_id, color" 
+  f2.puts messagesWithUserId
+end 
+
+puts colors.count
+
+f.close
