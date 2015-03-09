b0d32acafc813f4731df692f3bbf27f1d05e9503
RossKinsella <rosskinsella0@gmail.com>
2015-03-09 12:51:24 +0000

    code clean up

--- public/js/custom-scripts.js
+++ public/js/custom-scripts.js
@@ -1,28 +1,49 @@
 
 $(document).ready(function() {
-    states = {pageIsActive: 4, interaction_frequency: 20, current_time: 0};
+    // Globals
+    States = {pageIsActive: true};
     InteractionIndex = {datetime: 0, user_id: 1, color: 2};
-    var interactions = new Queue();
-    
+    ClockElements = { year: $('#year'), month: $('#month'), day: $('#day'), hour: $('#hour'), minute: $('#minute')};
+    Interactions = new Queue();
 
-    $(window).blur(function(){
-        states.pageIsActive = false;
-    });
+    Initialise();
 
-    $(window).focus(function(event) {
-       states.pageIsActive = true;
-    });
+    function Initialise()
+    {
+        // Pause state event listeners
+        $(window).blur(function(){
+            States.pageIsActive = false;
+        });
 
-    var time = 100;
+        $(window).focus(function(event) {
+           States.pageIsActive = true;
+        });
+
+        // Parse the data.
+        Papa.parse("data/testing_message_data.csv", {
+            download: true,
+            complete: function(results) {
+                // Add every interaction to a Queue
+                var curr = 1;
+                for (var i= 0; i < results.data.length - 1; i++)
+                {
+                        QueueInteraction((results.data[curr]));
+                        curr++;
+                }
+                // Start the main logic.
+                Main();
+            }
+        }); 
+    }
 
     function Main()
     {
         // Infinite loop
         window.setInterval(function () {
-            if (states.pageIsActive)
+            if (States.pageIsActive)
             {
                 // Dequeue an interaction and activate it
-                var interaction = interactions.dequeue();
+                var interaction = Interactions.dequeue();
                 ActivateInteraction(interaction);
             }
             else
@@ -36,15 +57,20 @@ $(document).ready(function() {
     {
         // Update the time
         datetime = interaction.datetime;
-        //console.log(datetime);
-        
-        yearElement.text(datetime.substr(0, 4));
-        monthElement.text(datetime.substr(5, 2));
-        dayElement.text(datetime.substr(8, 2));
-        hourElement.text(datetime.substr(11, 2));
-        minuteElement.text(datetime.substr(14, 2));  
+        UpdateTimer(datetime);
+
+        // Create the animation
+
     }
 
+    function UpdateTimer(datetime)
+    {
+        ClockElements.year.text(datetime.substr(0, 4));
+        ClockElements.month.text(datetime.substr(5, 2));
+        ClockElements.day.text(datetime.substr(8, 2));
+        ClockElements.hour.text(datetime.substr(11, 2));
+        ClockElements.minute.text(datetime.substr(14, 2));        
+    }
 
     function Interaction(radius, speed, width, xPos, yPos, datetime, user_id, color) {
         this.radius = radius;
@@ -58,7 +84,7 @@ $(document).ready(function() {
         this.opacity = .05 + Math.random() * .5;
     }
 
-    function addInteraction(interaction_data) {
+    function QueueInteraction(interaction_data) {
 
         var randomX = -10;
         var randomY = Math.round(-200 + Math.random() * canvas.height + 200);
@@ -76,33 +102,7 @@ $(document).ready(function() {
             interaction_data[InteractionIndex.color]
         );
         
-        interactions.enqueue(interaction);
-
+        Interactions.enqueue(interaction);
     }    
 
-    var count = 0;
-    var curr = 1;
-
-
-    var datetime;
-    var yearElement = $('#year');
-    var monthElement = $('#month');
-    var dayElement = $('#day');
-    var hourElement = $('#hour');
-    var minuteElement = $('#minute');
-
-    // Parse the data.
-    Papa.parse("data/testing_message_data.csv", {
-        download: true,
-        complete: function(results) {
-            // Add every interaction to a Queue
-            for (var i= 0; i < results.data.length - 1; i++)
-            {
-                    addInteraction((results.data[curr]));
-                    curr++;
-            }
-            // Start the main logic.
-            Main();
-        }
-    });
 });
