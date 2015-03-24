296a029e8f565638830d6025f884627159f029cd
RossKinsella <rosskinsella0@gmail.com>
2015-03-24 13:18:40 +0000

    fuck it- use a hidden embedded soundcloud

--- public/js/custom-scripts.js
+++ public/js/custom-scripts.js
@@ -54,11 +54,9 @@ $(document).ready(function() {
                 $('#loader-container').fadeOut( "slow" );
                 $('#clock').fadeIn( "slow" );
                 $('.info').fadeIn( "slow" );
-                $('#loader-container').append("<audio id = 'song' autoplay loop> <source src='data/The_Hummingbird.mp3' type='audio/mpeg'></audio>");
             
+                $('#loader-container').append("<iframe width= '100%' height= '450' scrolling= no' frameborder= 'no' src= 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/196109539&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>");
                 
-                
-
                 Main();
             }
         }); 
